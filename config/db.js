bb6dd8907d4e894fe1644c65d563499c9713aadf
Pramod Kumar <pramodkuumarteluri@gmail.com>
2026-06-07 18:52:19 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,20 +1,28 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
+
     if (err) {
         console.log("Database connection failed");
         console.log(err);
     } else {
         console.log("Database connected");
+
+        connection.release();
     }
+
 });
 
-module.exports = connection;
+module.exports = pool;
