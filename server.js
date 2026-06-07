d6d25feaa26496aab935e540d90b6dfd38ea1da2
Pramod Kumar <pramodkuumarteluri@gmail.com>
2026-06-07 19:18:37 +0530

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
-const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 
-const githubRoutes = require("./routes/githubRoutes.js");
+const githubRoutes = require("./routes/githubRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/github",githubRoutes)
+
+// Root Route
+
+app.get("/", (req, res) => {
+    res.send("GitHub Profile Analyzer API is Running");
+});
+
+
+// GitHub Routes
+
+app.use("/api/github", githubRoutes);
+
+
+// Server Start
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`);
-})
+});
